477ada9ff55bfa69883933ff67cd11c7979f0d04
Ammar <ammarelw@gmail.com>
2022-09-07 17:18:33 +0100

    playing with arrays

--- main.go
+++ main.go
@@ -5,16 +5,20 @@ import "fmt"
 func main(){
 	conferenceName := "Go Conference"
 	const conferenceTickets = 50
-	remainingTickets := 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("Weclome to our %v booking application \n", conferenceName)
 	fmt.Printf("We have total of %v Tickets and %v are still available \n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend!")
 
+	var bookings = []string{}
+
+	
+
 	var firstName string
 	var lasttName string
 	var email string
-	var userTicket int
+	var userTicket uint
 
 	// ask for their name
 	fmt.Println("Enter your first name:")
@@ -28,6 +32,12 @@ func main(){
 	fmt.Println("Enter number of tickets")
 	fmt.Scan(&userTicket)
 
+	remainingTickets = remainingTickets - userTicket
+	
+	bookings = append(bookings, firstName + " " + lasttName)
+
 	fmt.Printf("Thank you %v %v for booking %v tickets, you will recieve a confirmation email at %v \n",firstName, lasttName, userTicket, email)
 
+	fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceName)
+	fmt.Printf("All the bookings: %v \n", bookings)
 }
